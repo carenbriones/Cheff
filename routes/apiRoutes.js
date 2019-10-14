127477964eefebc791d3a8254008518045e227ae
Wendy Garcia <wendy.fern84@gmail.com>
2019-10-13 17:34:08 -0700

    routes and models

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,57 +1,110 @@
 var db = require("../models");
 
-// change examples to /api/recipes
-//  app.get
-//    /api/recipes
-//    /api/recipes/:category
-//    /api/recipes/:id
-//    /api/chef
-//    /api/chef/:id
-
-
-// app.post
-//    /api/recipes
-
-// app.delete
-//    /api/recipes/:id
-
 module.exports = function(app) {
-  // Get all examples
+
+// ---------- RECIPES ----------------//
+
+  // Get ALL recipes with their Chef and category
   app.get("/api/recipes", function(req, res) {
-    db.Recipe.findAll({}).then(function(dbRecipes) {
+    var query = {};
+    if (req.query.chef_id) {
+      query.ChefId = req.query.chef_id;
+    }
+    //??????????
+    if (req.query.category_id) {
+      query.CategoryId = req.query.category_id;
+    }
+    db.Recipe.findAll({
+      where: query,
+      include: [db.Chef, db.Category]
+    }).then(function(dbRecipes) {
       res.json(dbRecipes);
     });
   });
 
-  app.get("/api/recipes/:category", function(req, res) {
-    db.Recipe.findAll({
-      where: { category: req.params.category }
-    }).then(function(dbRecipes) {
-      res.json(dbRecipe);
-    });
-  });
-
+  // Get a recipe by id
   app.get("/api/recipes/:id", function(req, res) {
-    db.Recipe.findAll({
+    db.Recipe.findOne({
+      include: [db.Chef, db.Category],
       where: { category: req.params.id }
     }).then(function(dbRecipes) {
       res.json(dbRecipes);
     });
   });
-//-------------------------------------------//
-app.get("/api/chefs", function(req, res) {
-  db.Chef.findAll({}).then(function(dbChefs) {
-    res.json(dbChefs);
-  });
-});
 
-app.get("/api/chefs/:id", function(req, res) {
-  db.Chef.findAll({
-    where: { id: req.params.id }
-  }).then(function(dbChefs) {
-    res.json(dbChefs);
+//-----------CHEFS-----------------//
+
+  // Get all chefs and their recipes
+  app.get("/api/chefs", function(req, res) {
+    db.Chef.findAll({
+      include: db.Recipe
+    }).then(function(dbChefs) {
+      res.json(dbChefs);
+    });
   });
-});
+
+  // Get a Chef by their id
+  app.get("/api/chefs/:id", function(req, res) {
+    db.Chef.findAll({
+      include: db.Recipe,
+      where: { id: req.params.id }
+    }).then(function(dbChefs) {
+      res.json(dbChefs);
+    });
+  });
+
+//------- CATEGORIES ------------//
+  // Get all categories with their recipes
+  app.get("/api/categories", function(req, res) {
+    db.Category.findAll({
+      include: db.Recipe
+    }).then(function(dbCategorys) {
+      res.json(dbCategorys);
+    });
+  });
+
+  // Get one category by id with their recipes
+  app.get("/api/categories/:id", function(req, res) {
+    db.Category.findOne({
+      include: db.Recipe,
+      where: req.params.id
+    }).then(function(dbCategorys) {
+      res.json(dbCategorys);
+    });
+  });
+
+
+ //----------CREATE--------------//
+  // Create new recipe
+  app.post("/api/recipes", function(req, res) {
+    db.Recipe.create(req.body).then(function(dbRecipe) {
+      res.json(dbRecipe);
+    });
+  });
+  
+  // Create new chef
+  app.post("/api/chefs", function(req, res) {
+    db.Chef.create(req.body).then(function(dbChef) {
+      res.json(dbChef);
+    });
+  });
+
+//------------DELETE--------------//
+  // Delete a recipe by id
+  app.delete("/api/recipes/:id", function(req, res) {
+    db.Recipe.destroy({ where: { id: req.params.id } }).then(function(dbRecipes) {
+      res.json(dbRecipes);
+    });
+  });
+
+  // Delete a chef by id
+  app.delete("/api/chefs/:id", function(req, res) {
+    db.Chef.destroy({ where: { id: req.params.id } }).then(function(dbChefs) {
+      res.json(dbChefs);
+    });
+  });
+  
+};
 
 //-------------------------------------------//
 
@@ -61,50 +114,13 @@ app.get("/api/chefs/:id", function(req, res) {
 //     res.json(dbExamples);
 //   });
 // });
-//    /api/categories
 
-app.get("/api/categories", function(req, res) {
-  db.Category.findAll({}).then(function(dbCategorys) {
-    res.json(dbCategorys);
-  });
-});
-
-//    /api/categories/:id
-// ????????????????
-app.get("/api/categories/:id", function(req, res) {
-  db.Category.findAll({
-    where: req.params.id
-  }).then(function(dbCategorys) {
-    res.json(dbCategorys);
-  });
-});
-
-
-  // Create a new example
-  //-------------------------------//
-  app.post("/api/recipes", function(req, res) {
-    db.Recipe.create(req.body).then(function(dbRecipe) {
-      res.json(dbRecipe);
-    });
-  });
-
-  app.post("/api/chefs", function(req, res) {
-    db.Chef.create(req.body).then(function(dbChef) {
-      res.json(dbChef);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/recipes/:id", function(req, res) {
-    db.Recipes.destroy({ where: { id: req.params.id } }).then(function(dbRecipes) {
-      res.json(dbRecipes);
-    });
-  });
-  
-  app.delete("/api/chefs/:id", function(req, res) {
-    db.Recipes.destroy({ where: { id: req.params.id } }).then(function(dbRecipes) {
-      res.json(dbRecipes);
-    });
-  });
-  
-};
+  // //  Get recipes with their category and Chef~~ (find one)
+  // app.get("/api/recipes/:category", function(req, res) {
+  //   db.Recipe.findAll({
+  //     include: [db.Chef, db.Category],
+  //     where: { category: req.params.category }
+  //   }).then(function(dbRecipes) {
+  //     res.json(dbRecipe);
+  //   });
+  // });
